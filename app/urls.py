d7143616775f3817e23fdd332d094d619845cfa8
OmAxiani0 <aximaniom@gmail.com>
2023-05-15 23:19:58 +0530

    Migrated current version to Django

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+	path('', views.index, name='index'),
+    path('tools/', views.tools, name='tools'),
+	path('aboutus/', views.about_us, name='aboutus'),
+    
+	# Authentication
+	path('login/', views.log_in, name='log_in'),
+    path('signup/', views.sign_up, name='sign_up'),
+    path('logout/', views.log_out, name="log_out")
+]
---
+++ app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from app import utils
+
+def index(request):
+    return render(request, 'index.html')
+
+def tools(request):
+    if request.session.get('logged_in'):
+        context = {}
+        medicine = request.GET.get('medicine')
+        country = request.GET.get('country')
+        if medicine and country:
+            sale_prediction = utils.predict(medicine, country)
+            context = {'predict': sale_prediction}
+            return render(request, 'results.html', context)
+        elif medicine:
+            context = {'variables': utils.variables, 'countrys': utils.get_countrys(medicine)}
+            return render(request, 'tools.html', context)
+        context = {'variables': utils.variables }
+        return render(request, 'tools.html', context)
+    return redirect('/')
+
+def about_us(request):
+    return render(request, 'aboutus.html')
+
+def log_in(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.filter(username=email)
+        if user.exists():
+            user = User.objects.get(username=email)
+            if user.check_password(password):
+                request.session['logged_in'] = True
+                return redirect('/')
+            else:
+                context = {'msg': 'Invalid email or password'}
+                return render(request, 'auth/login.html', context)
+        else:
+            context = {'msg': 'Account does not exist!'}
+            return render(request, 'auth/login.html', context)
+    return render(request, 'auth/login.html')
+
+def sign_up(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.filter(username=email)
+        if user.exists():
+            context = {'msg': 'Account Already Registered! Please Login'}
+            return render(request, 'auth/signup.html', context)
+        user = User(username=email)
+        user.set_password(password)
+        user.save()
+        request.session['logged_in'] = True
+        return redirect('/')
+    return render(request, 'auth/signup.html')
+
+def log_out(request):
+    if request.session.get('logged_in'):
+        request.session.pop('logged_in')
+        return redirect('/')
+    return redirect('/login/')
